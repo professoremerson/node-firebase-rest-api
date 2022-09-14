1762daf1928fc9904b3defc91d812ff6f00a9d4e
joao <joaovsmoura@gmail.com>
2022-09-14 19:40:39 -0300

    Atualizando projeto

---
+++ models/pokemon.js
@@ -0,0 +1,15 @@
+// Definindo a classe Pokemon
+class Pokemon {
+  // Implementando o construtor da classe
+  constructor(id, number, name, type, description, weight, height) {
+    this.id = id
+    this.number = number
+    this.name = name
+    this.type = type
+    this.description = description
+    this.weight = weight
+    this.height = height
+  }
+}
+
+module.exports = Pokemon
