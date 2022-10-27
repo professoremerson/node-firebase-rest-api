a5ba8e79fe1c179fd14e5473317cee64de0e7f2f
Emerson Schafhauser <emerson@unc.br>
2022-10-26 22:16:38 -0300

    Criação dos métodos para autenticação, criação e validação do token

--- controllers/trainerController.js
+++ controllers/trainerController.js
@@ -3,6 +3,9 @@
 const trainerRepository = require('../repositories/trainer-repository')
 const ctrlBase = require('../bin/base/controller-base')
 const validators = require('../bin/lib/validators')
+const jwt = require('jsonwebtoken')
+const config = require('../config')
+
 const _repo = new trainerRepository()
 
 function trainerController() {}
@@ -39,6 +42,38 @@ trainerController.prototype.getById = async (req, res) => {
 
 trainerController.prototype.delete = async (req, res) => {
   ctrlBase.delete(_repo, req, res)
+}
+
+trainerController.prototype.authenticate = async (req, res) => {
+  let _validator = new validators()
+
+  _validator.isRequired(req.body.email, 'Informe o seu email')
+  _validator.isEmail(req.body.email, 'O email informado é inválido')
+  _validator.isRequired(req.body.password, 'Informe a sua senha')
+
+  if (!_validator.isValid()) {
+    res.status(400).send({
+      message: 'Não foi possível efetuar o Login!',
+      validation: _validator.errors()
+    })
+  }
+
+  let trainer = await _repo.authenticate(req.body.email, req.body.senha)
+  if (trainer) {
+    res.status(200).send({
+      trainer: trainer,
+      token: jwt.sign(
+        {
+          user: trainer
+        },
+        config.secretKey
+      )
+    })
+  } else {
+    res.status(404).send({
+      message: 'Usuário e senha inválidos!'
+    })
+  }
 }
 
 module.exports = trainerController
